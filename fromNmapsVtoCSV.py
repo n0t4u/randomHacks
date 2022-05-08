179073d71c5832ccea20537967b5fc313e210ac7
n0t4u <n0t4u@protonmail.com>
2022-05-08 20:28:33 +0200

    Added print options

--- fromNmapsVtoCSV.py
+++ fromNmapsVtoCSV.py
@@ -2,16 +2,20 @@
 # -*- coding: utf-8 -*-
 
 #Author: n0t4u
-#Version: 1.0
+#Version: 1.0.2
+
+#Notes
+#Add columns order option
+#Add remove unkown services
+#Add support for several IPs in the same file
+
 
 #imports
 import argparse
 import os
 import re
 import subprocess
-
-#Data
-outputFilename="nmap_parsed_open_ports.txt"
+from termcolor import colored
 
 #Functions
 def getIP(f):
@@ -20,32 +24,38 @@ def getIP(f):
 			#print(line)
 			if re.search(r'Host:[ ]*[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}',line):
 				ip = re.search(r'[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}',line)[0]
-				print(ip)
+				#print(ip)
 				return ip
 
 def parseData(file,ip):
-	command= "cat %s | grep -o -P '[\d]+\/open\/[\S]+\/\/([\S]+[\/]+)?([\S ]+[\/]+)?' | sed 's/[\/]\{1,\}/ /g' | sed 's/ , /\\n/g'" %file
+	command= "cat %s | grep -o -P '[\d]+\/open\/[\S]+\/\/([\S]+[\/]+)?([\S ]+[\/]+)?' | sed 's/[\/]\{1,\}/\t/g' | sed 's/\t, /\\n/g'" %file
 	proc= subprocess.Popen(command,shell=True, stdout=subprocess.PIPE)
 	parsedData= proc.stdout.read().decode("UTF-8")
 	#print(parsedData)
-	with open(outputFilename,"a+",encoding="UTF-8") as outputFile:
+	outputFilename="nmap_parsed_open_ports_%s.txt" %ip
+	if args.print:
 		for line in parsedData.rstrip("\n").split("\n"):
-			#print(ip,line)
-			dataline= "%s %s\n" %(ip,line)
-			outputFile.write(dataline) 
-	outputFile.close()
+			print(ip,line,sep="\t")
+	else:
+		with open(outputFilename,"w",encoding="UTF-8") as outputFile:
+			for line in parsedData.rstrip("\n").split("\n"):
+				#print(ip,line)
+				dataline= "%s\t%s\n" %(ip,line)
+				outputFile.write(dataline) 
+		outputFile.close()
 
 
 #Argparse
 parser= argparse.ArgumentParser()
 parser.add_argument("file",help="nmap sV .gnmap file to parse",nargs=1)
+parser.add_argument("-p","--print",dest="print", help="Print results instead of saving them into a file", action="store_true")
 args = parser.parse_args()
 
 #Main
 if __name__ == '__main__':
 	if os.path.splitext(args.file[0])[1] != ".gnmap":
-		print("[ERROR] gnmap file required")
+		print(colored("[ERROR]","red")," .gnmap file required")
 	else:
-		print(args.file[0])
+		#print(args.file[0])
 		ip =getIP(args.file[0])
 		parseData(args.file[0],ip)

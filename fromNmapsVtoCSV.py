520887aea724093d36bb48d5b4bc516c691941b6
n0t4u <n0t4u@protonmail.com>
2022-05-10 16:41:27 +0200

    Added support for several IPs

--- fromNmapsVtoCSV.py
+++ fromNmapsVtoCSV.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 
 #Author: n0t4u
-#Version: 1.0.3
+#Version: 1.1.0
 
 #Notes
 #Add columns order option
 #Add remove unkown services
-#Add support for several IPs in the same file
-
 
 #imports
 import argparse
@@ -32,45 +30,54 @@ def getIP(f):
 				#print(ip)
 				return ip
 
-def parseData(file,ip):
-	command= "cat %s | grep -o -P '[\d]+\/open\/[\S]+\/\/([\S]+[\/]+)?([\S ]+[\/]+)?' | sed 's/[\/]\{1,\}/\t/g' | sed 's/\t, /\\n/g'" %file
-	proc= subprocess.Popen(command,shell=True, stdout=subprocess.PIPE)
-	parsedData= proc.stdout.read().decode("UTF-8")
-	#print(parsedData)
-	for line in parsedData.rstrip("\n").split("\n"):
-		l = line.split("\t")
-		print(l)
-		try:
-			portData={
-				"ip":ip,
-				"port":l[0],
-				"state":l[1],
-				"protocol":l[2],
-				"service":l[3],
-				"version":l[4]
-			}
-		except Exception as e:
-			portData={
-				"ip":ip,
-				"port":l[0],
-				"state":l[1],
-				"protocol":l[2],
-				"service":l[3],
-				"version":"-"
-			}
-		else:
-			data.append(portData)
-
-
+def parseData(file):
+	try:
+		file = open(file,"r", encoding="UTF-8")
+		scan = file.read()
+		file.close()
+	except Exception as e:
+		raise e
+	else:
+		for ipScan in re.findall(r'Host: [\d\.]+[ \(\)]+\tPorts:[\S ]+',scan,re.I):
+			#print(ipScan)
+			host, ports = ipScan.split("\t")
+			ip =re.search(r'[\d\.]+',host)[0]
+			ports = ports.split(":")[1]
+			for port in re.sub(r'[\/]{1,}','/',ports).split('/,'):
+				p = port.split("/")
+				try:
+					portData={
+						"ip":ip,
+						"port":p[0].lstrip(" "),
+						"state":p[1],
+						"protocol":p[2],
+						"service":p[3],
+						"version":p[4]
+					}
+				except Exception as e:
+					portData={
+						"ip":ip,
+						"port":p[0].lstrip(" "),
+						"state":p[1],
+						"protocol":p[2],
+						"service":p[3],
+						"version":"-"
+					}
+				else:
+					data.append(portData)
+			generateOutput(ip)
+			data.clear()
+		#print(data)
+	return
 
 def generateOutput(ip):
 	if args.order:
 		order= args.order[0].split(",")
 	else:
-		order= [ip,port,protocol,status,service,version]
-	print(order)
+		order= ["ip","port","protocol","state","service","version"]
+	#print(order)
 	if args.print:
-		print("print")
+		#print("print")
 		for port in data:
 			try:
 				print(port[order[0]],port[order[1]],port[order[2]],port[order[3]],port[order[4]],port[order[5]], sep="\t")
@@ -83,10 +90,19 @@ def generateOutput(ip):
 	else:
 		outputFilename="nmap_parsed_open_ports_%s.txt" %ip
 		with open(outputFilename,"w",encoding="UTF-8") as outputFile:
-			for line in parsedData.rstrip("\n").split("\n"):
-				#print(ip,line)
-				dataline= "%s\t%s\n" %(ip,line)
-				outputFile.write(dataline) 
+			for port in data:
+				try:
+					output= ";".join([port[order[0]],port[order[1]],port[order[2]],port[order[3]],port[order[4]],port[order[5]]])
+					output = output+"\n"
+					outputFile.write(output)
+					#print(port[order[0]],port[order[1]],port[order[2]],port[order[3]],port[order[4]],port[order[5]], sep=";")
+				except KeyError as e:
+					print(colored("[Error] Unknown key %s.","red") %e, "Allowed values separated by comma: ip, port, protocol, state, service, version ")
+					sys.exit(0)
+#			for line in parsedData.rstrip("\n").split("\n"):
+#				#print(ip,line)
+#				dataline= "%s\t%s\n" %(ip,line)
+				#outputFile.write(dataline) 
 		outputFile.close()
 	return
 
@@ -104,7 +120,9 @@ if __name__ == '__main__':
 		print(colored("[ERROR]","red")," .gnmap file required")
 	else:
 		#print(args.file[0])
-		ip =getIP(args.file[0])
-		parseData(args.file[0],ip)
+		#ip =getIP(args.file[0])
+		#parseData(args.file[0],ip)
 		#print(data)
-		generateOutput(ip)
+		parseData(args.file[0])
+		#generateOutput()
+		#generateOutput(ip)

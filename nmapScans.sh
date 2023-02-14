c06b0658fab5b36c30ef18ba7fca57c731056d6e
n0t4u <n0t4u@protonmail.com>
2023-02-14 16:49:01 +0100

    Code improvements

--- nmapScans.sh
+++ nmapScans.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #nmapScans
 #Author: n0t4u
@@ -12,40 +12,39 @@ ColorOff='\033[0m'       # Text Reset
 
 #Default options
 options="-sT -T3 -Pn -n -v --open"
-scaned=()
-
+scanned=()
 
 nmap() {
 	echo -e "${Blue}[*] Nmap basic scan to ${1} with options ${2}${ColorOff}"
 	echo "${1} ${2}"
-	sudo nmap $2 -p- -oA nmap_p_$1 $1
-	sudo chown "$USER":"$USER" nmap_p_$1.*
+	sudo sh -c "nmap ${2} -p- -oA nmap_p_${1} ${1}"
+	sudo chown "$USER":"$USER" nmap_p_"${1}".*
 }
 
 getOpenPorts() {
-	ports=$(cat nmap_p_$1.gnmap | grep -P -o "[\d]+\/open\/[\S]+\/\/([\S]+[\/]{3,})?" | cut -d "/" -f 1 | sed ':a;N;$!ba;s/\n/,/g')
-	echo $ports
-	retval="${ports}"
+	ports=$(grep -P -o "[\d]+\/open\/[\S]+\/\/([\S]+[\/]{3,})?" nmap_p_"$1".gnmap | cut -d "/" -f 1 | sed ':a;N;$!ba;s/\n/,/g')
+	echo "${ports}"
 }
 
 nmapsV() {
 	echo -e "${Blue}[*] Nmap version scan to ${1} and ports ${2} with options ${3}${ColorOff}"
-	echo $1 $3
-	sudo nmap $3 -sV -sC -p $2 -oA nmap_sV_$1 $1
-	sudo chown "$USER":"$USER" nmap_sV_$1.*
+	echo "${1} ${3}"
+	sudo sh -c "nmap ${3} -sV -sC -p ${2} -oA nmap_sV_${1} ${1}"
+	sudo chown "$USER":"$USER" nmap_sV_"${1}".*
+	scanned+=("${1}")
 }
 
 trap_ctrlc() {
 	echo -e "${Red}[X] CTRL+C signal detected. Aborting port scanning...${ColorOff}"
 	echo -e "Assets fully scanned:\n"
-	for asset in "${scaned[@]}"; do
+	for asset in "${scanned[@]}"; do
 		echo "${asset}"
 	done
-	exit 0
+	exit 4
 }
 
 #Main
-trap "trap_ctrlc" SIGINT #2
+trap trap_ctrlc SIGINT SIGTERM #2
 
 if [ "$#" -lt 1 ] || [ "$#" -gt 3 ]; then
 	echo -e "${Red}[x] Only two arguments can be provided: File/IP and scan options (optional)${ColorOff}"
@@ -54,19 +53,28 @@ elif [ "$#" -eq 2 ]; then
 	options=$2
 fi
 echo -e "${Blue}Options${ColorOff}\t$2"
-if [ -f $1 ]; then 
+if [[ -f $1 ]]; then
 	echo -e "${Blue}File${ColorOff}\t$1"
-	while read ip; do
-		nmap $ip "${options}"
-		openPorts=$(getOpenPorts $ip)
-		nmapsV "${ip}" "${openPorts}" "${options}"
-	done < $1
+	while read -r ip; do
+		nmap "${ip}" "${options}"
+		openPorts=$(getOpenPorts "${ip}")
+		if [[ -z "${openPorts}" ]]; then
+			echo -e "${ip} has no open ports."
+			scanned+=("${ip}")
+		else
+			nmapsV "${ip}" "${openPorts}" "${options}"
+		fi
+	done < "$1"
 else
 	echo -e "${Blue}IP${ColorOff}\t$1"
-	nmap $1 "${options}"
-	openPorts=$(getOpenPorts $1)
-	echo -e "${openPorts}"
-	nmapsV "${1}" "${openPorts}" "${options}"
+	nmap "${1}" "${options}"
+	openPorts=$(getOpenPorts "$1")
+	if [[ -z "${openPorts}" ]]; then
+		echo -e "${1} has no open ports."
+	else
+		#echo -e "${openPorts}"
+		nmapsV "${1}" "${openPorts}" "${options}"
+	fi
 fi
 
 echo -e "${Green}Script finished sucessfully${ColorOff}"

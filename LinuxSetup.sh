32a9eba6f89f9466340936efdd0178845df2a37a
n0t4u <n0t4u@protonmail.com>
2023-06-29 16:51:55 +0200

    Updated LinuxSetup.sh

--- LinuxSetup.sh
+++ LinuxSetup.sh
@@ -54,6 +54,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo systemctl disable docker.service
+sudo systemctl start docker.service
 
 #Snap
 sudo apt install snapd
